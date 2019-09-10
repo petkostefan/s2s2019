d76c8d8fc15a45bb7648f23a4f7596236d294100
PaunMare <marko.paunovic1998@gmail.com>
2019-09-10 17:04:02 +0200

    partneri
    partneri :O

---
+++ js/partneri.js
@@ -0,0 +1,53 @@
+
+new Glide('.glide', {
+    type: 'carousel',
+    startAt: 0,
+    hoverpause: false,
+    perView: 4,
+    direction: 'rtl',
+    breakpoints:{
+      1024: {
+        perView: 3
+      },
+      600:{
+        perView: 2
+      }
+    },
+    autoplay: 3000
+  })
+  .mount()
+  new Glide('.partneri', {
+    type: 'carousel',
+    startAt: 0,
+    hoverpause: false,
+    perView: 4,
+    direction: 'rtl',
+    breakpoints:{
+      1024: {
+        perView: 3
+      },
+      600:{
+        perView: 2
+      }
+    },
+    autoplay: 3000
+  })
+
+.mount()
+new Glide('.mediji', {
+    type: 'carousel',
+    startAt: 0,
+    hoverpause: false,
+    perView: 4,
+    direction: 'rtl',
+    breakpoints:{
+      1024: {
+        perView: 3
+      },
+      600:{
+        perView: 2
+      }
+    },
+    autoplay: 3000
+  })
+.mount()
